dd44732537e4a023c639307137b372c0a90ecd49
Владислав <voloshenko_vladislav@mail.ru>
2024-07-29 14:54:29 +0700

    add dictionaries mocks

---
+++ src/mocks/handlers/auth.ts
@@ -0,0 +1,84 @@
+import { nanoid } from '@reduxjs/toolkit';
+import { http, HttpResponse } from 'msw';
+
+
+export const authHandler = http.post("/api-admin/v1/auth", async ({ request}) => {
+  const token = nanoid();
+  const body = await request.json() as { email: string | null, password: string | null };
+  const email = body.email;
+  const password = body.password;
+
+  let data = null;
+  let errors: null | { email?: string[], password?: string[] } = null
+  let status: number = 200;
+
+  if (
+    email === 'mail@mail.ru'
+    && password === '123'
+  ) {
+    data = {
+      access_token: token,
+      token_type: "Bearer",
+      expires: "2025-05-06 07:06:28"
+    };
+  } else {
+    errors = {};
+    
+    if (!email) {
+      errors.email = ["The email field is required."];
+    } else if (email !== 'mail@mail.ru') {
+      errors.email = ["Invalid email."];
+    }
+
+    if (!password) {
+      errors.password = ["The password field is required."];
+    } else if (password !== '123') {
+      errors.password = ["Invalid password."];
+    }
+
+    status = 422;
+  }
+
+  return new HttpResponse(
+    JSON.stringify({
+      data,
+      errors,
+      error_message: null,
+    }),
+    { status },
+  )
+});
+
+export const meHandler = http.get("/api-admin/v1/me", () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: {
+        id: 1,
+        name: 'Владслав Волощенко',
+        email: 'mail@mail.ru',
+        authorized_at: "2022-02-08 06:20:58",
+        expires: "2022-05-09 06:20:58",
+        permissions: [
+          "newsCreate",
+          "newsView",
+          "newsEdit",
+          "newsDelete"
+        ],
+      },
+      errors: null,
+      error_message: null,
+    }),
+    { status: 200 },
+  )
+});
+
+export const logoutHandler = http.get("/api-admin/v1/logout", () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: null,
+      errors: null,
+      error_message: null,
+    }),
+    { status: 200 },
+  )
+});
---
+++ src/mocks/handlers/dictionaries.ts
@@ -0,0 +1,122 @@
+import { http, HttpResponse } from 'msw';
+
+
+export const topicsHandler = http.get("/api-admin/v1/topics", () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: {
+        items: [
+          {
+            id: 1,
+            title: "Архитектура"
+          },
+          {
+            id: 2,
+            title: "Политика"
+          },
+          {
+            id: 3,
+            title: "Погода"
+          },
+          {
+            id: 4,
+            title: "Спорт"
+          },
+          {
+            id: 5,
+            title: "Экономика"
+          },
+          {
+            id: 6,
+            title: "Экология"
+          },
+        ]
+      },
+      error_message: null,
+      errors: null
+    }),
+    { status: 200 },
+  )
+});
+
+export const statusesHandler = http.get("/api-admin/v1/statuses", () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: {
+        items: [
+          {
+            id: 1,
+            title: "Черновик"
+          },
+          {
+            id: 2,
+            title: "Опубликовано"
+          },
+          {
+            id: 3,
+            title: "Запланировано"
+          },
+        ]
+      },
+      error_message: null,
+      errors: null
+    }),
+    { status: 200 },
+  )
+});
+
+export const typesHandler = http.get("/api-admin/v1/types", () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: {
+        items: [
+          {
+            id: 1,
+            title: "Без типа"
+          },
+          {
+            id: 2,
+            title: "Новость"
+          },
+          {
+            id: 3,
+            title: "Срочное"
+          },
+          {
+            id: 4,
+            title: "Фотоновость"
+          },
+        ]
+      },
+      error_message: null,
+      errors: null
+    }),
+    { status: 200 },
+  )
+});
+
+export const bannerPlacesHandler = http.get("/api-admin/v1/banner_places", () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: {
+        items: [
+          {
+            id: 1,
+            title: "Сквозной в шапке"
+          },
+          {
+            id: 2,
+            title: "Под текстом новости"
+          },
+          {
+            id: 3,
+            title: "Сквозной всплывающий баннер"
+          },
+        ]
+      },
+      error_message: null,
+      errors: null
+    }),
+    { status: 200 },
+  )
+});
